5cacc5862bcacea1e157bc7937f22edeadf7c7c5
milothea <sauchanka.liubou@gmail.com>
2021-04-08 00:26:07 +0300

    feat: implement function for cheching a MAC-address

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,8 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  return (/^([0-9a-f]{2}-){5}[0-9a-f]{2}$/i).test(n);
 }
 
 module.exports = isMAC48Address;
