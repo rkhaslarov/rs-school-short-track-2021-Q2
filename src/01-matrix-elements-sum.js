2a65b9f94c90a59a5e1b665c72bc95c6b69ed3b4
HelenKolesnikovich <solnechnaya_88@list.ru>
2021-04-08 15:34:56 +0300

    feat: implement task #1

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,36 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+  let isZero = false;
+
+  for(let i = 1; i < matrix.length; i++)
+  {
+    for(let j = 0; j < matrix[i].length; j++)
+    {
+      if(matrix[i][j] === 0)
+      {
+        isZero = true;
+        for(let k = i-1; k > -1; k--)
+        {
+          if(matrix[k][j] !== 0)
+          {
+            sum += matrix[k][j];
+          }
+        }
+      }
+    }
+  }
+  if(!isZero) {
+    for(let i = 0; i < matrix.length; i++)
+    {
+      matrix[i].forEach(function(value) {
+        sum += value;
+      });
+    }
+  }
+  return sum;
 }
 
 module.exports = getMatrixElementsSum;
