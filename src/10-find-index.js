2835e8d01da3b6594f6ad3b6ec1e3f0cfd27b297
milothea <sauchanka.liubou@gmail.com>
2021-04-09 23:57:55 +0300

    feat: implement search algorithm

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,21 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  const searchArea = [0, array.length];
+  let currentIndex = Math.floor(array.length / 2);
+
+  while (array[currentIndex] !== value) {
+    if (value < array[currentIndex]) {
+      searchArea[1] = currentIndex;
+      currentIndex = Math.floor(currentIndex / 2);
+    } else {
+      searchArea[0] = currentIndex;
+      currentIndex = Math.floor((searchArea[1] - searchArea[0]) / 2 + searchArea[0]);
+    }
+  }
+
+  return currentIndex;
 }
 
 module.exports = findIndex;
