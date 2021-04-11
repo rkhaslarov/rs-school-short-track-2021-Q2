488935fabaed8e623e50e1898928e25514e56065
milothea <sauchanka.liubou@gmail.com>
2021-04-11 09:38:46 +0300

    feat: implement lines encoder

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,33 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  let acc = {};
+  let encodedLine = '';
+
+  for (let i = 0; i < arr.length; i++) {
+    const prev = arr[i - 1];
+    const cur = arr[i];
+
+    if (i === 0 || cur === prev || cur in acc) {
+      acc[cur] = (acc[cur]) ? acc[cur] + 1 : 1;
+    }
+
+    if ((cur !== prev && i !== 0) || i === arr.length) {
+      acc[prev] = (acc[prev] === 1) ? '' : acc[prev];
+      encodedLine += `${acc[prev]}${Object.keys(acc)[0]}`;
+      acc = {};
+      acc[cur] = (acc[cur]) ? acc[cur] + 1 : 1;
+    }
+
+    if (i === arr.length - 1) {
+      acc[cur] = (acc[cur] === 1) ? '' : acc[cur];
+      encodedLine += `${acc[cur]}${Object.keys(acc)[0]}`;
+    }
+  }
+
+  return encodedLine;
 }
 
 module.exports = encodeLine;
