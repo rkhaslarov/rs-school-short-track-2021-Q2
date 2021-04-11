30c73fd3dbd346e72d1e5d39338355a0326c7386
HelenKolesnikovich <solnechnaya_88@list.ru>
2021-04-11 15:03:06 +0300

    feat: implement the solution to the task '11-encode-line.js'

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,28 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  if (str.length === 0) {
+    return '';
+  }
+  let resultStr = '';
+  const chars = str.split('');
+  chars.sort();
+  for (let i = 0; i < chars.length; i++) {
+    let amountChars = 1;
+    for (let j = i + 1; j < chars.length; j) {
+      if (chars[i] === chars[j]) {
+        amountChars++;
+        chars.splice(j, 1);
+      } else {
+        break;
+      }
+    }
+    const resultAmount = amountChars > 1 ? amountChars : '';
+    const resultChar = `${resultAmount}${chars[i]}`;
+    resultStr += resultChar;
+  }
+  return resultStr;
 }
 
 module.exports = encodeLine;
