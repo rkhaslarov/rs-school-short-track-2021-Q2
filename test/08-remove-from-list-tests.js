77c78ebe953ba72dc032dfd2b70b76e9c46bc712
milothea <sauchanka.liubou@gmail.com>
2021-04-11 23:13:39 +0300

    refactor: rectore deleted test

---
+++ test/08-remove-from-list-tests.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const ListNode = require('../extensions/list-node');
+const removeKFromList = require('../src/08-remove-from-list');
+it.optional = require('../extensions/it-optional');
+
+function convertArrayToList(arr) {
+  return arr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur);
+      node.next = acc;
+      return node;
+    }
+
+    return new ListNode(cur);
+  }, null);
+}
+
+describe('08-remove-from-list', () => {
+  it.optional('should return the list without values equal to k', () => {
+    const initial = convertArrayToList([3, 1, 2, 3, 4, 5]);
+    const expected = convertArrayToList([1, 2, 4, 5]);
+    assert.deepEqual(removeKFromList(initial, 3), expected);
+  });
+});
