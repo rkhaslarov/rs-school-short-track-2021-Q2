0d25bc7d4b701ee51f14379507befc77fc63c43e
milothea <sauchanka.liubou@gmail.com>
2021-04-11 20:39:17 +0300

    feat: implement linked list editor

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -7,7 +7,7 @@
  * @return {List}
  *
  * @example
- * For l = [3, 1, 2, 3, 4, 5] and l = 3,
+ * For l = [3, 1, 2, 3, 4, 5] and k = 3,
  * the output should be [1, 2, 4, 5]
  *
  * Singly - linked lists are already defined with this interface
@@ -17,8 +17,28 @@
  * }
  */
 
-function removeKFromList(/* l, k */) {
-  throw new Error('Not implemented');
+function removeKFromList(l, k) {
+  let list = l;
+  let curr = list;
+
+  while (curr.value === k) {
+    list = curr.next;
+    curr = curr.next;
+  }
+
+  let prev = list;
+
+  while (prev.next !== null) {
+    if (curr.value === k) {
+      prev.next = curr.next;
+    } else {
+      prev = curr;
+    }
+
+    curr = curr.next;
+  }
+
+  return list;
 }
 
 module.exports = removeKFromList;
