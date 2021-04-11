fedb867ac48f8d3e8cd41a4aa4a5460a682c52a6
HelenKolesnikovich <solnechnaya_88@list.ru>
2021-04-11 15:28:38 +0300

    feat: implement solution for the task '15-get-email-domain.js'

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,9 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const emailSignIndex = email.lastIndexOf('@');
+  return email.substring(emailSignIndex + 1);
 }
 
 module.exports = getEmailDomain;
