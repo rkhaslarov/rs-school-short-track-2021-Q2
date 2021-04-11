52b35aedfddb24a2272c939d61c924070e4a7df1
milothea <sauchanka.liubou@gmail.com>
2021-04-11 23:14:57 +0300

    feat: implement queue class

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,4 +1,4 @@
-// const ListNode = require('../extensions/list-node');
+const ListNode = require('../extensions/list-node');
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
@@ -12,16 +12,54 @@
  */
 
 class Queue {
-  get size() {
-    throw new Error('Not implemented');
+  constructor() {
+    this.list = {};
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  get size() {
+    let size = 1;
+
+    while (this.list.next !== null) {
+      size += 1;
+    }
+
+    return size;
+  }
+
+  enqueue(element) {
+    const node = new ListNode(element);
+
+    if (this.list.value === undefined) {
+      this.list = node;
+
+      return true;
+    }
+
+    if (this.list.next === null) {
+      this.list.next = node;
+    } else {
+      let lastNode = this.list.next;
+
+      while (lastNode.next != null) {
+        lastNode = lastNode.next;
+      }
+
+      lastNode.next = node;
+    }
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    const topNode = this.list.value;
+
+    if (this.list.next === null) {
+      this.list = {
+        value: null,
+      };
+    } else {
+      this.list = this.list.next;
+    }
+
+    return topNode;
   }
 }
 
